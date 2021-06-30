3e983470aeb5a695f1a86b70d8681307bd26157a
Alexandre Bensimon <alexandre@tryriot.com>
2021-06-30 23:12:10 +0200

    Init project

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    L'accord parfait est <span class="text-green-600">en cours</span> de
+    construction...
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
