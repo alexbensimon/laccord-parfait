fdc4e6c25a7bed1422f82a923b26c4ed2b4d952f
Alexandre Bensimon <alexandre@tryriot.com>
2021-08-19 12:36:29 +0200

    Make image gallery 3 squared tiles

--- components/slices/ImageGallery.vue
+++ components/slices/ImageGallery.vue
@@ -1,14 +1,14 @@
 <template>
-  <section class="image-gallery content-section">
+  <section class="content-section">
     <prismic-rich-text :field="slice.primary.gallery_title" />
-    <div class="gallery">
-      <div v-for="item in slice.items" :key="item.id" class="gallery-item">
+    <div class="px-3 flex flex-wrap justify-between">
+      <div v-for="item in slice.items" :key="item.id" class="w-72 h-96">
         <component
           :is="item.link.id ? 'prismic-link' : 'div'"
           :field="item.link"
         >
-          <prismic-image :field="item.image" />
-          <prismic-rich-text :field="item.image_description" />
+          <prismic-image class="w-72 h-72" :field="item.image" />
+          <!-- <prismic-rich-text :field="item.image_description" /> -->
           <p class="uppercase text-center mt-2">
             {{ $prismic.asText(item.link_label) }}
           </p>
@@ -41,35 +41,3 @@ export default class ImageGallery extends Vue {
   @Prop({ required: true }) slice!: ImageGallerySliceType;
 }
 </script>
-
-<style lang="sass" scoped>
-.gallery
-  display: -webkit-box
-  // OLD - iOS 6-, Safari 3.1-6, BB7
-  display: -ms-flexbox
-  // TWEENER - IE 10
-  display: -webkit-flex
-  // NEW - Safari 6.1+. iOS 7.1+, BB10
-  display: flex
-  -webkit-flex-wrap: wrap
-  flex-wrap: wrap
-  -webkit-justify-content: space-between
-  justify-content: space-between
-
-.gallery-item
-  -webkit-box-flex: 0 1 48%
-  -moz-box-flex: 0 1 48%
-  -webkit-flex: 0 1 48%
-  -ms-flex: 0 1 48%
-  flex: 0 1 48%
-
-@media (max-width: 767px)
-  .content-section
-    margin-bottom: 2rem
-  .gallery-item
-    -webkit-box-flex: 100%
-    -moz-box-flex: 100%
-    -webkit-flex: 100%
-    -ms-flex: 100%
-    flex: 100%
-</style>
